6938f691a664125c321b201a3b928318e99ed8fc
Erick de Almeida Oliveira Riso <eaor@WINDOWS.CIN.UFPE.BR>
2018-11-26 17:11:53 -0300

    Correções adicionais e simplificações

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ console.log(satsolver.solve("tutorial.cnf"))
 console.log(satsolver.solve("simple0.cnf"))
 console.log(satsolver.solve("simple1.cnf"))
 console.log(satsolver.solve("simple2.cnf"))
+console.log(satsolver.solve("hole1.cnf"))
+console.log(satsolver.solve("hole4.cnf"))
+
+
